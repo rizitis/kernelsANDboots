591cb0c41237c20c91c8c282c435a60f9111dcfb
Anagnostakis Ioannis <412135+rizitis@users.noreply.github.com>
2023-08-14 15:11:40 +0300

    Add files via upload

---
+++ aaa_kernels.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+# Anagnostakis Ioannis GR Crete 8/2023
+# This script ran every time new kernel release is to be installaded in a Slackware system via upgradepkg...
+# So lets name it aaa_kernels
+# The idea is that  upgradepkg dont touch entries in nitrd.gz
+# so only vmlinuz* files need to be restored after upgradepkg upgrade kernel
+# and /lib/modules/old_kernel_version
+
+# Redistribution and use of this script, with or without modification, is
+# permitted provided that the following conditions are met:
+#
+# 1. Redistributions of this script must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+#
+#  THIS SOFTWARE IS PROVIDED BY THE AUTHOR "AS IS" AND ANY EXPRESS OR IMPLIED
+#  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+#  MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO
+#  EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+#  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+#  PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+#  OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+#  WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+#  OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+#  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+if [ "$EUID" -ne 0 ];then
+echo "ROOT ACCESS PLEASE OR GO HOME..."
+exit 1
+fi
+
+KERNEL2="$(uname -r)"
+FILE1=vmlinuz-
+DIR=/boot/BAK2
+DIR1=/boot
+DIR3=/lib/modules
+
+if [ -d "$DIR" ]
+then
+echo "aaa_kernels: NO UPDATES"
+else
+echo "$KERNEL2"
+# mkdir -p not needed , if needed then something is going wrong
+echo "First time aaa_kernels ran here"
+echo "or kernel was upgraded."
+echo "Creating back-up files..."
+mkdir "$DIR" || exit
+wait
+# Taking vmlinuz-* backup
+cp "$DIR1"/"$FILE1"* "$DIR"/ 
+wait
+# backup modules 
+cp -a "$DIR3"/"$KERNEL2" "$DIR"/ 
+wait
+rm "$DIR"/*-huge
+echo ""
+echo "aaa_kernels DB CREATED OK"
+echo ""
+fi
+
+
---
+++ zzz_kernels.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+
+# This script must ran after upgradepkg upgrade kernel , after autoslack-initrd AND 
+# if aaa_kernels.sh is installed too, else this script have no use.
+# So lets name it zzz_kernels
+# initrd.gz keeps old entries after upgradepkg upgrade kernel so its ok
+# only vmlinuz-(huge,generic)-old_kernel_version need to be backup and /lib/modules 
+# NOTE: grub-mkconfig must run after this script add command at the end of this script 
+# or if you have slackup-grub installed then slackup-grub must ran last.
+
+# root
+if [ "$EUID" -ne 0 ];then
+echo "ROOT ACCESS PLEASE OR GO HOME..."
+exit 1
+fi
+
+KERNEL2="$(uname -r)"
+FILE=vmlinuz-
+DIR=/boot/BAK2
+DIR1=/boot
+DIR3=/lib/modules
+dir=/usr/local/zzz_kernels
+file=/usr/local/zzz_kernels/zzz_kernels.TXT
+file2=/usr/local/zzz_kernels/zzz_kernels.BAK
+
+
+if [ -d "$dir" ]
+then
+echo "zzz_kernels in progress"
+else
+# this is only for the first time... so -p not needed , if needed then something going wrong
+echo "Looks like its the first time zzz_kernels ran here"
+mkdir "$dir" || exit
+/bin/ls -tr /var/log/pkgtools/removed_scripts/ | grep kernel | tail -2 > "$file"
+echo "DONE"
+exit
+fi
+
+echo "$KERNEL2"
+cd "$dir" || exit
+if [ -f /usr/local/zzz_kernels/zzz_kernels.TXT ]
+then 
+mv "$file" "$file2" || exit
+else 
+echo "************************************"
+echo "Something went wrong, ATTENSION... *"
+echo "************************************"
+exit
+fi
+
+set -e
+
+if [ -f "$file2" ]
+then 
+/bin/ls -tr /var/log/pkgtools/removed_scripts/ | grep kernel | tail -2 > "$file"
+fi
+
+if cmp -s zzz_kernels.TXT zzz_kernels.BAK ; then
+echo "zzz_kernels: NO KERNEL UPDATE WAS FOUND"
+exit
+elif
+echo "zzz_kernels: KERNEL WAS UPDATED, DOING STUFF..."
+then
+cp "$DIR1"/"$FILE*" "$DIR"/
+cp -a "$DIR1"/"$KERNEL2" "$DIR3"
+rm -r "$DIR"
+echo ""
+echo "zzz_kernels finish, OK"
+echo ""
+else
+ echo "********************************************************"
+echo "Something went wrong!"
+echo "Before shutdown check you have"
+echo "a Linux-kernel installed a valid initrd and update-grub"
+echo "********************************************************"
+exit
+fi
+
+# Here in can be placed update-grub command
+# I have slackup-grub for that job, skip this step
+# grub-mkconfig -o /boot/grub/grub.cfg
